aabdc433c3ab0cb98e7cdc3f2fc06f09c27a9fb2
Raja Rao DV <rajar@vmware.com>
2012-03-28 00:36:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var host = process.env.VCAP_APP_HOST || "localhost";
+var port = process.env.VCAP_APP_PORT || "3000";
+
+var express = require('express');
+var app = module.exports = express.createServer();
+
+	//load express configurations
+	var expressConfig = require('./lib/express_config');
+	expressConfig(app);
+
+	//load routes (after configurations)
+	var routes  = require('./lib/routes');
+	routes(app);
+	app.listen(port, host);		
